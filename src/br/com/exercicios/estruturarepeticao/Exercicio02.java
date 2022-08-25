9fa6b2fff2518a3fa553b456c2418f3eb06b840e
GustavoWendel <gustavo_wendel@hotmail.com>
2022-08-25 20:53:34 -0300

    Exercícios já feitos

---
+++ src/br/com/exercicios/estruturarepeticao/Exercicio02.java
@@ -0,0 +1,11 @@
+package br.com.exercicios.estruturarepeticao;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        int soma = 0;
+        for (int contador = 1; contador <= 15; contador++) {
+            soma = soma + contador;
+        }
+        System.out.println(soma);
+    }
+}
