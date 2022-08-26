e590d930a539c38864fb68a5431f22ebe3230948
GustavoWendel <gustavoborges@sisconsultoria.com.br>
2022-08-26 12:12:50 -0300

    Exercício 05

--- src/br/com/exercicios/estruturarepeticao/Exercicio05.java
+++ src/br/com/exercicios/estruturarepeticao/Exercicio05.java
@@ -1,7 +1,20 @@
 package br.com.exercicios.estruturarepeticao;
 
+import java.util.Scanner;
+
 public class Exercicio05 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numero = 1;
+        int soma = 0;
 
+        while(numero <= 10) {
+
+            System.out.println("Digite um valor: ");
+            numero = scan.nextInt();
+            soma = soma + numero;
+        }
+
+        System.out.println(soma);
     }
 }
