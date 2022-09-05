6e06495a4b5df0cb961c9688f4cc84d6e93a1222
GustavoWendel <gustavoborges@sisconsultoria.com.br>
2022-09-05 12:10:55 -0300

    Exercício 12

---
+++ src/br/com/exercicios/estruturarepeticao/Exercicio12.java
@@ -0,0 +1,29 @@
+package br.com.exercicios.estruturarepeticao;
+
+/*Escreva um algoritmo que leia 20 números do usuário e
+exiba quantos números são pares*/
+
+import java.util.Scanner;
+
+public class Exercicio12 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int numero = 0;
+        int contador = 0;
+        int par = 0;
+
+
+        do {
+            System.out.println();
+            numero = scan.nextInt();
+            contador++;
+
+            if(numero % 2 == 0) {
+                par++;
+            }
+        } while (contador <= 5);
+
+        System.out.println("Quantidade números pares = " + par);
+    }
+}
