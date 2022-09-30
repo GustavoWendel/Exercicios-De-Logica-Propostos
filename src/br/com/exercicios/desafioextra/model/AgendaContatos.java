08eec01454b25414db0b514a6301ba2f11c4dd36
Gustavo Wendel <gustavo_wendel@hotmail.com>
2022-09-30 12:05:04 -0300

    Desafio AgendaContatos

--- src/br/com/exercicios/desafioextra/model/AgendaContatos.java
+++ src/br/com/exercicios/desafioextra/model/AgendaContatos.java
@@ -1,7 +1,32 @@
 package br.com.exercicios.desafioextra.model;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 public class AgendaContatos {
 
-    private String nome;
-    private String telefone;
+    private final Map<String, String> contatos = new HashMap<>();
+
+    public Map<String, String> getContatos() {
+        return contatos;
+    }
+
+    public void adicionar(String nome, String telefone) {
+        contatos.put(nome, telefone);
+    }
+
+    public String consultar(String nome) {
+        return contatos.get(nome).toLowerCase(Locale.ROOT);
+    }
+
+    public String pesquisarContato(String telefone) {
+        for (Map.Entry<String, String> entry: contatos.entrySet()) {
+            if (entry.getValue().equals(telefone)) {
+                return entry.getKey();
+            }
+        }
+        return telefone;
+    }
+
 }
